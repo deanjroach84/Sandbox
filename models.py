42f861ff69a8d87d97f15f56fa51a72adc282727
deanjroach84 <dean.djroach.roach2@gmail.com>
2025-08-10 17:10:45 +0100

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
@@ -17,6 +17,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
 
 class Scan(db.Model):
     __tablename__ = 'scans'
@@ -26,7 +27,7 @@ class Scan(db.Model):
     target_ip = db.Column(db.String(100), nullable=False)
     start_port = db.Column(db.Integer, nullable=False)
     end_port = db.Column(db.Integer, nullable=False)
-    open_ports = db.Column(db.Text)  # Stored as comma-separated string or JSON
+    open_ports = db.Column(db.Text)  # JSON string of open ports
     scan_date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
