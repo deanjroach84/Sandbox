b49c7c77626362e6f9942c37cec6c0d1f0e65ab0
deanjroach84 <dean.djroach.roach2@gmail.com>
2025-08-12 06:18:58 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
+from wtforms.validators import DataRequired, Length, EqualTo, Email, IPAddress, NumberRange
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
@@ -11,11 +11,16 @@ class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
 class ScanForm(FlaskForm):
-    target_ip = StringField('Target IP', validators=[DataRequired()])
-    start_port = IntegerField('Start Port', validators=[DataRequired()])
-    end_port = IntegerField('End Port', validators=[DataRequired()])
+    target_ip = StringField('Target IP', validators=[DataRequired(), IPAddress(ipv4=True, message="Invalid IPv4 address")])
+    start_port = IntegerField('Start Port', validators=[DataRequired(), NumberRange(min=1, max=65535, message="Port must be between 1 and 65535")])
+    end_port = IntegerField('End Port', validators=[DataRequired(), NumberRange(min=1, max=65535, message="Port must be between 1 and 65535")])
     submit = SubmitField('Start Scan')
+
+class AdminAddUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=30)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Add User')
