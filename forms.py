7eb653c3f002dcbc6b482d549001d96a54f46bed
deanjroach84 <dean.djroach.roach2@gmail.com>
2025-08-12 03:12:59 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,44 +1,22 @@
+# forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
+    identifier = StringField("Username or Email", validators=[DataRequired(), Length(min=2, max=120)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField("Login")
 
 class RegisterForm(FlaskForm):
-    username = StringField(
-        'Username',
-        validators=[DataRequired(), Length(min=3, max=25)]
-    )
-    email = StringField(
-        'Email',
-        validators=[DataRequired(), Email()]
-    )
-    password = PasswordField(
-        'Password',
-        validators=[DataRequired(), Length(min=6)]
-    )
-    confirm_password = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired(), EqualTo('password')]
-    )
-    submit = SubmitField('Register')
-
+    username = StringField("Username", validators=[DataRequired(), Length(min=3, max=64)])
+    email = StringField("Email", validators=[DataRequired(), Email(), Length(max=120)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Register")
 
 class ScanForm(FlaskForm):
-    target = StringField(
-        'Target IP or Domain',
-        validators=[DataRequired()]
-    )
-    start_port = IntegerField(
-        'Start Port',
-        validators=[DataRequired(), NumberRange(min=1, max=65535)]
-    )
-    end_port = IntegerField(
-        'End Port',
-        validators=[DataRequired(), NumberRange(min=1, max=65535)]
-    )
-    submit = SubmitField('Start Scan')
+    target = StringField("Target IP or Domain", validators=[DataRequired(), Length(min=3, max=255)])
+    start_port = IntegerField("Start Port", validators=[DataRequired(), NumberRange(min=1, max=65535)])
+    end_port = IntegerField("End Port", validators=[DataRequired(), NumberRange(min=1, max=65535)])
+    submit = SubmitField("Start Scan")
